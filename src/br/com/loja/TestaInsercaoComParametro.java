e1281166d8eaa3dfbbe3b016dbb833654c72591b
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-02-15 21:17:03 -0300

    adicionado metódo adicionarVariavel

--- src/br/com/loja/TestaInsercaoComParametro.java
+++ src/br/com/loja/TestaInsercaoComParametro.java
@@ -8,24 +8,31 @@ import java.sql.Statement;
 
 public class TestaInsercaoComParametro {
 		public static void main(String[] args) throws SQLException {
-			String nome = "MOUSE); delete from produto";
-			double preco = 99;
 			
 			ConnectionFactory factory = new ConnectionFactory();
 			Connection connection = factory.recuperarConexao();
 			
+			String nome = "MOUSE); delete from produto";
+			double preco = 99;
+			
 			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, PRECO) VALUES (?, ?)" , Statement.RETURN_GENERATED_KEYS);
 			
-			stm.setString(1, nome);
-			stm.setDouble(2, preco);		
-			stm.execute();
-
-			ResultSet rst = stm.getGeneratedKeys();
+			ResultSet rst = adicionarVariavel(nome, preco, stm);
 			
 			while(rst.next()) {
 				int id = rst.getInt(1);
 				System.out.println("O id criado foi " + id);
 			}
+			connection.close();
+		}
 
+		private static ResultSet adicionarVariavel(String nome, double preco, PreparedStatement stm)
+				throws SQLException {
+			stm.setString(1, nome);
+			stm.setDouble(2, preco);		
+			stm.execute();
+
+			ResultSet rst = stm.getGeneratedKeys();
+			return rst;
 		}
 }
