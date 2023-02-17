ac53ba35fa7bdd58f00ec12945c3a736dcf01d84
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-02-17 12:58:45 -0300

    adicionado bloco try/catch para realizar o rollback

--- src/br/com/loja/TestaInsercaoComParametro.java
+++ src/br/com/loja/TestaInsercaoComParametro.java
@@ -8,31 +8,44 @@ import java.sql.Statement;
 
 public class TestaInsercaoComParametro {
 		public static void main(String[] args) throws SQLException {
-			
+		
 			ConnectionFactory factory = new ConnectionFactory();
 			Connection connection = factory.recuperarConexao();
+			connection.setAutoCommit(false); // não permite fazer o autoCommit
 			
-			String nome = "MOUSE); delete from produto";
-			double preco = 99;
-			
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, PRECO) VALUES (?, ?)" , Statement.RETURN_GENERATED_KEYS);
-			
-			ResultSet rst = adicionarVariavel(nome, preco, stm);
-			
-			while(rst.next()) {
-				int id = rst.getInt(1);
-				System.out.println("O id criado foi " + id);
+			try {
+				PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, PRECO) VALUES (?, ?)" , Statement.RETURN_GENERATED_KEYS);
+				
+				adicionarVariavel("Teclado", 300, stm);
+				adicionarVariavel("fones", 200, stm);
+				
+				connection.commit(); // responsável por commitar caso a transação dê certo
+				
+				connection.close();
+				
+				stm.close();				
+			} catch(Exception e) { // a exception nesse caso é utilizada caso para fazer o rollback, não permitindo inconsistência 
+				e.printStackTrace();
+				System.out.println("Rollback executado.");
+				connection.rollback();
 			}
-			connection.close();
 		}
 
-		private static ResultSet adicionarVariavel(String nome, double preco, PreparedStatement stm)
+		private static void adicionarVariavel(String nome, double preco, PreparedStatement stm)
 				throws SQLException {
 			stm.setString(1, nome);
 			stm.setDouble(2, preco);		
 			stm.execute();
-
+			
+			if(nome.equals("fones")) {
+				throw new RuntimeException("Não foi possível adicionar o produto.");
+			}
 			ResultSet rst = stm.getGeneratedKeys();
-			return rst;
+			while(rst.next()) {
+				int id = rst.getInt(1);
+				System.out.println("O id criado foi " + id);
+			}
+			rst.close();;
 		}
 }
+
